447909a4a64d723c86a14ee54739b6e90b6b2f65
Deepak Kumar <sh2deepakkumar@gmail.com>
2020-12-16 13:40:50 +0100

    reorder pt > 50

--- ExoPieAnalyzer/monoHbbAnalyzer.py
+++ ExoPieAnalyzer/monoHbbAnalyzer.py
@@ -426,15 +426,17 @@ def runbbdm(txtfile):
 
 	    nBjets_notiso_index =[]
             tmp_index = [ij for ij in range(ep_THINnJet) if (ep_THINjetDeepCSV[ij] > MWP and abs(ak4jeteta[ij]) < 2.5)]
+            isleadJetPt50Present = False
 	    for ij in range(len(tmp_index)):
-		if ij==0 and ak4jetpt[tmp_index[0]] > 50.0:
-		    nBjets_notiso_index.append(tmp_index[0])
-		if ij!=0:nBjets_notiso_index.append(tmp_index[ij])
-            
+		if ak4jetpt[tmp_index[ij]] > 50.0:isleadJetPt50Present = True
+		if not isleadJetPt50Present:continue
+		nBjets_notiso_index.append(tmp_index[ij])
+
+            nBjets_notiso = len(nBjets_notiso_index) 
 	    if nBjets_notiso==2 and len(tmp_index)>2:
 		print "before", tmp_index,'ak4jetpt',ak4jetpt
 	        print "after",nBjets_notiso_index
-            nBjets_notiso = len(nBjets_notiso_index)
+            
 
             ak4_pt30_eta4p5  = [True for ij in range(ep_THINnJet)] #pt > 30 and eta < 4.5 is already applied at skimmer level
             fjet_pt200_eta_2p5 = [True for ij in range(ep_nfjet)] #pt > 200 and eta < 2.5 is already applied at skimmer level
